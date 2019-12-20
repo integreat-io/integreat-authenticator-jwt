26b6747ee49771eb9c40549bb15a79d67eea1c85
Kjell-Morten <post@kjellmorten.no>
2019-12-20 13:52:52 +0100

    Write test for error logging

--- src/tests/import.test.ts
+++ src/tests/import.test.ts
@@ -1,11 +1,35 @@
 import test from 'ava'
+import sinon = require('sinon')
 
 import resourcesFn from '..'
 
-const resources = resourcesFn()
-
 test('should have resources', t => {
+  const resources = resourcesFn()
   t.truthy(resources)
   t.truthy(resources.authenticators)
   t.truthy(resources.authenticators.jwt)
+})
+
+test('should log error', async t => {
+  const logger = {
+    info: sinon.stub(),
+    error: sinon.stub()
+  }
+  const resources = resourcesFn(logger)
+  const options = {
+    audience: 'waste-iq',
+    key: 's3cr3t',
+    expiresIn: 'invalid' // To make signing fail
+  }
+  const request = {
+    action: 'GET',
+    params: {},
+    data: null,
+    access: { ident: { id: 'johnf' } }
+  }
+
+  const ret = await resources.authenticators.jwt.authenticate(options, request)
+
+  t.is(ret.status, 'refused')
+  t.is(logger.error.callCount, 1)
 })
